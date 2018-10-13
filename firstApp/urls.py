4c416e610e649b17b30b0eda902f1fd8e8bb2a75
papanosuke <cbf17819@nifty.com>
2018-10-13 20:23:07 +0900

    On branch master  Changes to be committed: 	modified:   firstApp/__pycache__/urls.cpython-35.pyc 	modified:   firstApp/__pycache__/views.cpython-35.pyc 	new file:   firstApp/templates/cards.html 	new file:   firstApp/templates/display_cards.html 	modified:   firstApp/templates/name.html 	modified:   firstApp/urls.py 	modified:   firstApp/views.py

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^hello/$',views.hello),
     url(r'^card/$',views.card),
     url(r'^welcome/$',views.welcome),
+    url(r'^cards/$',views.cards),
+    url(r'^random_cards/$',views.random_cards),
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,24 +1,41 @@
 from django.shortcuts import render
 
-#add2018.08.19 start
 from django.http import HttpResponse
-#add2018.08.19 end
 
 # Create your views here.
-#add2018.08.19 start
 def hello(request):
     return HttpResponse('こんにちは')
-#add2018.08.19 end
-#add2018.08.25 start
 def card(request):
     return render(request,'card.html')
+#
 def welcome(request):
-    class person(object):
-        def __init__(self, name, sex="famale")
-            self.name = name
-            self.sex = sex 
-            
-    me = person("みずはら","Male")
-    dictionary = {"person" : me}
+    name = "田中"
+    dictionary = {"name" : name}
     return render(request,'name.html',dictionary)
-#add2018.08.25 end
+#
+def cards(request):
+    rank_str = []
+    rank = list(range(1,14))
+    for x in rank:
+        rank_str.append(str(x).zfill(2))
+#
+    return render(request,"cards.html",{"card_rank":rank_str})
+#
+def random_cards(request):
+#
+    import random
+    suits = ["S","H","D","C"]
+    ranks = range(1,14)
+    deck = [(x,y) for x in ranks for y in suits]
+    random.shuffle(deck)
+#
+    card1 = deck.pop()
+    card2 = deck.pop()
+#
+    dictionary = {
+        "suit" : card1[1],
+        "rank" : str(card1[0]).zfill(2),
+        "suit2" : card2[1],
+        "rank2" : str(card2[0]).zfill(2),
+   }
+    return render(request,"display_cards.html",dictionary)

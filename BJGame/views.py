3cf2dee8f906e081f7911fca4f4b76de0f6a50fa
papanosuke <cbf17819@nifty.com>
2019-01-14 15:22:11 +0900

    On branch master  Changes to be committed: 	new file:   BJGame/__pycache__/__init__.cpython-35.pyc 	new file:   BJGame/__pycache__/admin.cpython-35.pyc 	new file:   BJGame/__pycache__/models.cpython-35.pyc 	new file:   BJGame/migrations/__pycache__/__init__.cpython-35.pyc 	modified:   BJGame/views.py 	modified:   firstDjango/__pycache__/settings.cpython-35.pyc

--- BJGame/views.py
+++ BJGame/views.py
@@ -32,3 +32,44 @@ def game(request):
         dictionary.update(csrf(request))
         
         return render(request,"bjgame.html",dictionary)
+    elif request.method == "POST":
+        token = request.session["token"]
+        deck = r.get_redis(token,"deck")
+        money = r.get_redis(token,"money")
+        
+        player_hands = r.get_redis(token,"player_hands")
+        dealer_hands = r.get_redis(token,"dealer_hands")
+        
+        if (r.get_redis(token,"game_now") == False):
+            r.set_redis(token,"game_now",True)
+            money -= int(request.POST["bet"]
+            r.set_redis(token,"bet",int(request.POST["bet"]))
+            dealer_hands = []
+            dealer_hands.append(deck.pop())
+            player_hands = []
+            player_hands.append(deck.pop())
+            player_hands.append(deck.pop())
+        
+            r.set_redis(token,"dealer_hands",dealer_hands)
+            r.set_redis(token,"player_hands",player_hands)
+            
+            dealer_point = bj.get_point(dealer_hands)
+            player_point = bj.get_point(player_hands)
+            
+            r.set_redis(token,"deck",deck)
+
+            dictionary = {
+                "msg" : "選択してください。",
+                "dealer_cards" : dealer_hands,
+                "dealer_point" : dealer_point,
+                "player_cards" : player_hands,
+                "player_point" : player_point,
+                "able_bet" : True,
+                "able_double" : True,
+                "money" : money,
+                "dealer_cards" : int(request.POST["bet"],
+            }
+            dictionary.update(csrf(request))
+        
+            return render(request,"bjgame.html",dictionary)
+    

695a61a3078a93beb9fc6b0802d78420ae58f119
papanosuke <cbf17819@nifty.com>
2019-01-19 14:34:35 +0900

    On branch master  Changes to be committed: 	new file:   BJGame/__pycache__/blackjack.cpython-35.pyc 	new file:   BJGame/__pycache__/redis_helper.cpython-35.pyc 	modified:   BJGame/__pycache__/urls.cpython-35.pyc 	new file:   BJGame/__pycache__/views.cpython-35.pyc 	deleted:    BJGame/files 	new file:   BJGame/redis_helper.py 	new file:   BJGame/static/cards/C_01.png 	new file:   BJGame/static/cards/C_02.png 	new file:   BJGame/static/cards/C_03.png 	new file:   BJGame/static/cards/C_04.png 	new file:   BJGame/static/cards/C_05.png 	new file:   BJGame/static/cards/C_06.png 	new file:   BJGame/static/cards/C_07.png 	new file:   BJGame/static/cards/C_08.png 	new file:   BJGame/static/cards/C_09.png 	new file:   BJGame/static/cards/C_10.png 	new file:   BJGame/static/cards/C_11.png 	new file:   BJGame/static/cards/C_12.png 	new file:   BJGame/static/cards/C_13.png 	new file:   BJGame/static/cards/D_01.png 	new file:   BJGame/static/cards/D_02.png 	new file:   BJGame/static/cards/D_03.png 	new file:   BJGame/static/cards/D_04.png 	new file:   BJGame/static/cards/D_05.png 	new file:   BJGame/static/cards/D_06.png 	new file:   BJGame/static/cards/D_07.png 	new file:   BJGame/static/cards/D_08.png 	new file:   BJGame/static/cards/D_09.png 	new file:   BJGame/static/cards/D_10.png 	new file:   BJGame/static/cards/D_11.png 	new file:   BJGame/static/cards/D_12.png 	new file:   BJGame/static/cards/D_13.png 	new file:   BJGame/static/cards/H_01.png 	new file:   BJGame/static/cards/H_02.png 	new file:   BJGame/static/cards/H_03.png 	new file:   BJGame/static/cards/H_04.png 	new file:   BJGame/static/cards/H_05.png 	new file:   BJGame/static/cards/H_06.png 	new file:   BJGame/static/cards/H_07.png 	new file:   BJGame/static/cards/H_08.png 	new file:   BJGame/static/cards/H_09.png 	new file:   BJGame/static/cards/H_10.png 	new file:   BJGame/static/cards/H_11.png 	new file:   BJGame/static/cards/H_12.png 	new file:   BJGame/static/cards/H_13.png 	new file:   BJGame/static/cards/S_01.png 	new file:   BJGame/static/cards/S_02.png 	new file:   BJGame/static/cards/S_03.png 	new file:   BJGame/static/cards/S_04.png 	new file:   BJGame/static/cards/S_05.png 	new file:   BJGame/static/cards/S_06.png 	new file:   BJGame/static/cards/S_07.png 	new file:   BJGame/static/cards/S_08.png 	new file:   BJGame/static/cards/S_09.png 	new file:   BJGame/static/cards/S_10.png 	new file:   BJGame/static/cards/S_11.png 	new file:   BJGame/static/cards/S_12.png 	new file:   BJGame/static/cards/S_13.png 	new file:   BJGame/static/cards/card_bk_blue.png 	new file:   BJGame/static/cards/card_bk_red.png 	new file:   BJGame/static/cards/table_bk.png 	new file:   BJGame/static/css/bootstrap-theme.css 	new file:   BJGame/static/css/bootstrap-theme.css.map 	new file:   BJGame/static/css/bootstrap-theme.min.css 	new file:   BJGame/static/css/bootstrap.css 	new file:   BJGame/static/css/bootstrap.css.map 	new file:   BJGame/static/css/bootstrap.min.css 	new file:   BJGame/static/fonts/glyphicons-halflings-regular.eot 	new file:   BJGame/static/fonts/glyphicons-halflings-regular.svg 	new file:   BJGame/static/fonts/glyphicons-halflings-regular.ttf 	new file:   BJGame/static/fonts/glyphicons-halflings-regular.woff 	new file:   BJGame/static/js/bootstrap.js 	new file:   BJGame/static/js/bootstrap.min.js 	new file:   BJGame/static/js/npm.js 	new file:   BJGame/static/python_bk_s_new.png 	new file:   BJGame/static/style.css 	new file:   BJGame/templates/LICENSE 	new file:   BJGame/templates/README.md 	new file:   BJGame/templates/bjgame2.html 	new file:   BJGame/templates/gameover.html 	new file:   BJGame/templates/howto.html 	modified:   BJGame/urls.py 	modified:   BJGame/views.py 	modified:   db.sqlite3

--- BJGame/urls.py
+++ BJGame/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^game/$",views.game),
+    url(r"^game/$",views.game,name = "game"),
+    url(r"^howto/$",views.howto),
 ]
--- BJGame/views.py
+++ BJGame/views.py
@@ -5,6 +5,13 @@ import random
 import BJGame.redis_helper as r
 import BJGame.blackjack as bj
 
+def conv_image_path(list):
+    tmp = []
+    for i in list:
+        s = str(i[1]) + "_" + str(i[0]).zfill(2) + ".png"
+        tmp.append(s)
+    return tmp
+
 def game(request):
     if request.method == "GET":
         token = str(random.random())
@@ -29,6 +36,7 @@ def game(request):
             "money" : 100,
         }
         dictionary.update(csrf(request))
+        return render(request,"bjgame2.html",dictionary)
         
         return render(request,"bjgame.html",dictionary)
     elif request.method == "POST":
@@ -41,9 +49,26 @@ def game(request):
         
         if (r.get_redis(token,"game_now") == False):
             
+            try:
+                if money < int(request.POST["bet"]):
+                    raise ValueError("too large bet")
+            except ValueError:
+                msg = "正しくベットしてください。"
+                dictionary = {
+                    "msg" : msg,
+                    "dealer_cards" : [],
+                    "dealer_point" : 0,
+                    "player_cards" : [],
+                    "player_point" : 0,
+                    "able_bet" : True,
+                    "money" : money,
+                }
+                dictionary.update(csrf(request))
+                return render(request,"bjgame2.html",dictionary)
+            
             r.set_redis(token,"game_now", True)
-            money -= int(request.POST["bet"]
-            r.set_redis(token,"money",money)
+            money -= int(request.POST["bet"])
+            r.set_redis(token,"money", money)
             r.set_redis(token,"bet",int(request.POST["bet"]))
             dealer_hands = []
             dealer_hands.append(deck.pop())
@@ -61,17 +86,17 @@ def game(request):
 
             dictionary = {
                 "msg" : "選択してください。",
-                "dealer_cards" : dealer_hands,
+                "dealer_cards" : conv_image_path(dealer_hands),
                 "dealer_point" : dealer_point,
-                "player_cards" : player_hands,
+                "player_cards" : conv_image_path(player_hands),
                 "player_point" : player_point,
-                "able_bet" : True,
+                "able_bet" : False,
                 "able_double" : True,
                 "money" : money,
-                "bet" : int(request.POST["bet"],
+                "bet" : int(request.POST["bet"]),
             }
             dictionary.update(csrf(request))
-            return render(request,"bjgame.html",dictionary)
+            return render(request,"bjgame2.html",dictionary)
         else:
             op = request.POST["operation"]
             bet = r.get_redis(token,"bet")
@@ -95,15 +120,15 @@ def game(request):
                 player_point = bj.get_point(player_hands)
                 msg,money = bj.win_lose(dealer_hands,player_hands,bet,money)
                 if money <= 0:
-                    return HttpResponse("GameOver")
+                    return render(request,"gameover.html")
                 r.set_redis(token,"money",money)
                 msg += "　ベットしてください。　"
                 
                 dictionary = {
                     "msg" : msg,
-                    "dealer_cards" : dealer_hands,
+                    "dealer_cards" : conv_image_path(dealer_hands),
                     "dealer_point" : bj.get_point(dealer_hands),
-                    "player_cards" : player_hands,
+                    "player_cards" : conv_image_path(player_hands),
                     "player_point" : bj.get_point(player_hands),
                     "able_bet" : True,
                     "able_double" : True,
@@ -115,18 +140,21 @@ def game(request):
                 r.set_redis(token,"deck",deck)
                 r.set_redis(token,"game_now",False)
                 
-                return render(request,"bjgame.html",dictionary)
+                return render(request,"bjgame2.html",dictionary)
             else:
                 r.set_redis(token,"deck",deck)
                 
                 dictionary = {
-                    "dealer_cards" : dealer_hands,
+                    "dealer_cards" : conv_image_path(dealer_hands),
                     "dealer_point" : bj.get_point(dealer_hands),
-                    "player_cards" : player_hands,
+                    "player_cards" : conv_image_path(player_hands),
                     "player_point" : player_point,
                     "able_bet" : False,
                     "money" : money,
                     "bet" : bet,
                 }
                 dictionary.update(csrf(request))
-                return render(request,"bjgame.html",dictionary)
+                return render(request,"bjgame2.html",dictionary)
+
+def howto(request):
+    return render(request,"howto.html")
